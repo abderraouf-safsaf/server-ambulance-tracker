760ae2c71b00ad2c55b6ec21ea8aec34f3cb3832
abderraouf-safsaf <fa_safsaf@esi.dz>
2020-03-13 13:34:51 -0500

    Fix random formula

--- src/app.js
+++ src/app.js
@@ -4,19 +4,7 @@ import { env, mongo, port, ip, apiRoot } from "./config";
 import express from "./services/express";
 import api from "./api";
 
-const io = require("socket.io")(
-  http /*{
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-      "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
-  }
-}*/
-);
+const io = require("socket.io")(http);
 
 const app = express(apiRoot, api);
 const server = http.createServer(app);
@@ -25,9 +13,6 @@ app.use(cors());
 
 //mongoose.connect(mongo.uri);
 //mongoose.Promise = Promise;
-io.on("connection", function(socket) {
-  console.log("a user connected");
-});
 
 setImmediate(() => {
   const s = server.listen(port, ip, () => {
@@ -38,43 +23,53 @@ setImmediate(() => {
       env
     );
 
-    //runIo();
+    runIo();
   });
   io.listen(s);
 });
 
-const AMBULANCE_LOCATIONS = {
-  1: { lng: 123, lat: 456 }
+let AMBULANCE_LOCATIONS = {
+  1: { lat: 32.7766642, lng: -96.7969879 }
+};
+
+app.post("/turnon", (req, res, next) => {
+  const { lat, lng } = req.body;
+  AMBULANCE_LOCATIONS[Object.keys(AMBULANCE_LOCATIONS).length + 1] = {
+    lat,
+    lng
+  };
+  io.emit("allLocations", AMBULANCE_LOCATIONS);
+  res.status(200).send();
+});
+
+app.post("/updateLocation/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { lat, lng } = req.body;
+  AMBULANCE_LOCATIONS[id] = { lat, lng };
+  io.emit("allLocations", AMBULANCE_LOCATIONS);
+  res.status(200).send();
+});
+
+const simulateMoving = () => {
+  setInterval(() => {
+    Object.keys(AMBULANCE_LOCATIONS).map(index => {
+      const location = AMBULANCE_LOCATIONS[index];
+      AMBULANCE_LOCATIONS[index] = {
+        lat: location.lat + Math.random() / 5000,
+        lng: location.lng + Math.random() / 5000
+      };
+    });
+    io.emit("allLocations", AMBULANCE_LOCATIONS);
+  }, 1000);
 };
 
 const runIo = () => {
-  //io.set("origins", "*:*");
   io.on("connection", socket => {
-    console.log("user connected");
-    let previousId;
-    const safeJoin = currentId => {
-      socket.leave(previousId);
-      socket.join(currentId);
-      previousId = currentId;
-    };
-
-    socket.on("getDoc", docId => {
-      safeJoin(docId);
-      socket.emit("document", documents[docId]);
+    console.log(" connected");
+    socket.on("getAllLocations", () => {
+      socket.emit("allLocations", AMBULANCE_LOCATIONS);
     });
-
-    socket.on("addDoc", doc => {
-      documents[doc.id] = doc;
-      safeJoin(doc.id);
-      io.emit("documents", Object.keys(documents));
-      socket.emit("document", doc);
-    });
-
-    socket.on("editDoc", doc => {
-      documents[doc.id] = doc;
-      socket.to(doc.id).emit("document", doc);
-    });
-    io.emit("documents", Object.keys(documents));
+    simulateMoving();
   });
 };
 
