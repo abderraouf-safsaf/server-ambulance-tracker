a7ccd23d36176fe39d98bae1a9a4ceed2a4ffe1c
abderraouf-safsaf <fa_safsaf@esi.dz>
2020-03-13 09:43:46 -0500

    Integrate socket.io

--- src/app.js
+++ src/app.js
@@ -1,19 +1,63 @@
-import http from 'http'
-import { env, mongo, port, ip, apiRoot } from './config'
-import mongoose from './services/mongoose'
-import express from './services/express'
-import api from './api'
+import http from "http";
+import { env, mongo, port, ip, apiRoot } from "./config";
+//import mongoose from "./services/mongoose";
+import express from "./services/express";
+import api from "./api";
 
-const app = express(apiRoot, api)
-const server = http.createServer(app)
+const io = require("socket.io")(http);
 
-mongoose.connect(mongo.uri)
-mongoose.Promise = Promise
+const app = express(apiRoot, api);
+const server = http.createServer(app);
+
+//mongoose.connect(mongo.uri);
+//mongoose.Promise = Promise;
 
 setImmediate(() => {
   server.listen(port, ip, () => {
-    console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
-  })
-})
+    console.log(
+      "Express server listening on http://%s:%d, in %s mode",
+      ip,
+      port,
+      env
+    );
 
-export default app
+    runIo();
+  });
+});
+
+const AMBULANCE_LOCATIONS = {
+  1: { lng: 123, lat: 456 }
+};
+
+const runIo = () => {
+  io.on("connection", socket => {
+    console.log("user connected");
+    let previousId;
+    const safeJoin = currentId => {
+      socket.leave(previousId);
+      socket.join(currentId);
+      previousId = currentId;
+    };
+
+    socket.on("getDoc", docId => {
+      safeJoin(docId);
+      socket.emit("document", documents[docId]);
+    });
+
+    socket.on("addDoc", doc => {
+      documents[doc.id] = doc;
+      safeJoin(doc.id);
+      io.emit("documents", Object.keys(documents));
+      socket.emit("document", doc);
+    });
+
+    socket.on("editDoc", doc => {
+      documents[doc.id] = doc;
+      socket.to(doc.id).emit("document", doc);
+    });
+
+    io.emit("documents", Object.keys(documents));
+  });
+};
+
+export default app;
